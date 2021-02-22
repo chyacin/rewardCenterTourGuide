dfa2bd5481995a429eb02b6dd1aab8d29843eb91
chyacin <calvinhyacinth@gmail.com>
2021-02-22 14:25:15 +0100

    Created the rewards central app

---
+++ src/main/java/com/openclassroom/rewardCenterTourGuide/RewardCenterTourGuideApplication.java
@@ -0,0 +1,13 @@
+package com.openclassroom.rewardCenterTourGuide;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RewardCenterTourGuideApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RewardCenterTourGuideApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/openclassroom/rewardCenterTourGuide/RewardCenterTourGuideApplicationTests.java
@@ -0,0 +1,13 @@
+package com.openclassroom.rewardCenterTourGuide;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RewardCenterTourGuideApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
